408d61d39e874082983f57423530d44547d4ed8c
kb12233 <kaybeeocfemia@gmail.com>
2023-05-02 21:17:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/csit284_f1_idonote/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.csit284_f1_idonote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
